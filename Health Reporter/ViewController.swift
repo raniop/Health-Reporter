95b377cd1acbce3f72fab131305dcf5ca56fa4a4
Rani Ophir <raniop@mac.com>
2026-01-24 15:35:07 +0200

    Initial Commit

---
+++ Health Reporter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Health Reporter
+//
+//  Created by Rani Ophir on 24/01/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Health ReporterTests/Health_ReporterTests.swift
@@ -0,0 +1,17 @@
+//
+//  Health_ReporterTests.swift
+//  Health ReporterTests
+//
+//  Created by Rani Ophir on 24/01/2026.
+//
+
+import Testing
+@testable import Health_Reporter
+
+struct Health_ReporterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
